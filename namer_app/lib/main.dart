e3b62ff9540bafe0f86fe9704a65c96b2aadf7f7
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2023-07-11 12:51:14 +0530

    favorites page to be added

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -48,7 +49,122 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+// ...
+
+class MyHomePage extends StatefulWidget {
+  
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+
+}
+
+    return LayoutBuilder( // LayoutBuilder's builder callback is called every time the constraints change
+      builder: (context, constraints ) {
+        return Scaffold(
+          body: Row( // ha s two children SafeArea anfd Expanded
+            children: [
+              SafeArea(   // ensures that its child is not obsured ny a hardware notch or a status bar
+                child: NavigationRail(
+                  extended: constraints.maxWidth >=600 , // so that the app responds to its environment , such as screen size , orientation and platform
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    
+                    setState(() {
+                      selectedIndex = value;   // when the oneDestinationSelected callback is called , instead of merely printing the new value to console, oyu assign it to 
+                      // selectedIndex inside a setState() call
+                      //this call is similar to notifyListeners() method used previously 
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -80,7 +196,7 @@ class MyHomePage extends StatelessWidget {
                   icon: Icon(icon),
                   label: Text('Like'),
                 ),
-                
+
                 SizedBox(width: 10),
                 ElevatedButton(
                   onPressed: (){
@@ -95,7 +211,7 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
-}
+
 
 class BigCard extends StatelessWidget {
   const BigCard({
